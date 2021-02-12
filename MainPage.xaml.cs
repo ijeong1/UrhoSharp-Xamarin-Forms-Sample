486e6e9294f5a03e8ed0422d8c611be5b93e0234
Ilnam Jeong <jeong.il.nam@gmail.com>
2021-02-12 12:56:35 -0500

    Initial commit

---
+++ MainPage.xaml.cs
@@ -0,0 +1,79 @@
+﻿using Urho;
+using Urho.Forms;
+using Xamarin.Forms;
+
+namespace UrhoSharpSample
+{
+    public partial class MainPage : ContentPage
+    {
+        UrhoSurface urhoSurface;
+        Charts urhoApp;
+        Slider heightSlider;
+        Slider widthSlider;
+        Slider depthSlider;
+
+        public MainPage()
+        {
+            InitializeComponent();
+
+            urhoSurface = new UrhoSurface();
+            urhoSurface.VerticalOptions = LayoutOptions.FillAndExpand;
+
+            heightSlider = new Slider(0, 5, 2.5);
+            heightSlider.ValueChanged += OnHeightValuesSliderValueChanged;
+
+            widthSlider = new Slider(0, 5, 2.5);
+            widthSlider.ValueChanged += OnWidthValuesSliderValueChanged;
+
+            depthSlider = new Slider(0, 5, 2.5);
+            depthSlider.ValueChanged += OnDepthValuesSliderValueChanged;
+
+            Title = " UrhoSharp + Xamarin.Forms";
+            Content = new StackLayout
+            {
+                Padding = new Thickness(12, 12, 12, 40),
+                VerticalOptions = LayoutOptions.FillAndExpand,
+                Children = {
+                    urhoSurface,
+                    new Label { Text = "Height (in):" },
+                    heightSlider,
+                    new Label { Text = "Width (in):" },
+                    widthSlider,
+                    new Label { Text = "Depth (in):" },
+                    depthSlider
+                }
+            };
+        }
+
+        protected override void OnDisappearing()
+        {
+            UrhoSurface.OnDestroy();
+            base.OnDisappearing();
+        }
+
+        protected override async void OnAppearing()
+        {
+            base.OnAppearing();
+
+            //await HelloWorldUrhoSurface.Show<Charts>(new ApplicationOptions(assetsFolder: null) { Orientation = ApplicationOptions.OrientationType.LandscapeAndPortrait });
+            urhoApp = await urhoSurface.Show<Charts>(new ApplicationOptions(assetsFolder: null) { Orientation = ApplicationOptions.OrientationType.LandscapeAndPortrait });
+
+        }
+
+        void OnHeightValuesSliderValueChanged(System.Object sender, Xamarin.Forms.ValueChangedEventArgs e)
+        {
+            urhoApp.SelectedBar.HeightValue = (float)e.NewValue;
+        }
+
+        void OnWidthValuesSliderValueChanged(System.Object sender, Xamarin.Forms.ValueChangedEventArgs e)
+        {
+            urhoApp.SelectedBar.WidthValue = (float)e.NewValue;
+        }
+
+        void OnDepthValuesSliderValueChanged(System.Object sender, Xamarin.Forms.ValueChangedEventArgs e)
+        {
+            urhoApp.SelectedBar.DepthValue = (float)e.NewValue;
+        }
+
+    }
+}
---
+++ README.md
@@ -0,0 +1,2 @@
+# UrhoSharpSample
+ Example of 3D graphic control in Xamarin Forms using urhoSharp.
